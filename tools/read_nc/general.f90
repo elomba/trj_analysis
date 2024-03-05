f03db427e54047db8e165cc697b8cb12c0339ab7
Enrique Lomba <enrique.lomba@gmail.com>
2024-03-05 11:40:21 +0100

    Adding read_nc tool

---
+++ tools/read_nc/general.f90
@@ -0,0 +1,6 @@
+module g_types
+  ! Define general types
+  integer, parameter :: double = selected_real_kind(15)
+  integer, parameter :: float = selected_real_kind(6)
+  integer, parameter :: myprec = float
+end module g_types
