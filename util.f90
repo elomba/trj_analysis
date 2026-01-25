954c47c8f663a51b15f3166d1ab09d40f0f7854e
Enrique Lomba <enrique.lomba@gmail.com>
2026-01-25 11:20:13 +0100

    V0.7.4 Version implementing maximum no. of neighbors checking

--- util.f90
+++ util.f90
@@ -113,7 +113,7 @@ contains
       implicit none
       logical, intent(in) :: use_cell,run_clusters,run_dyn, confined
       integer, intent(in) :: nmol
-      if (use_cell) call cells_init_pre_nc_read(nmol)
+      if (use_cell.or.run_order) call cells_init_pre_nc_read(nmol)
       if (run_clusters) call clusters_init(nmol)
       if (run_dyn) call dyn_init()
       if (confined) call prof_init()
